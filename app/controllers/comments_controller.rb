1e3e88a6b34d3a763e219ab0a51f5b7c6c6dc570
genagain <ohta.g@husky.neu.edu>
2015-04-06 20:45:21 -0400

    implemented comment error message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,11 @@ class CommentsController < ApplicationController
   def create
     @talk = Talk.find(params[:talk_id])
     @comment = Comment.new(talk_id: @talk.id, content: params[:comment][:content])
-    @comment.save
-    flash[:notice] = 'Thanks for commenting'
+    if @comment.save
+      flash[:notice] = 'Thanks for commenting'
+    else
+      flash[:notice] = "Comment can't be blank"
+    end
     redirect_to talk_path(@talk)
   end
 
--- spec/features/comments/user_creates_comment_spec.rb
+++ spec/features/comments/user_creates_comment_spec.rb
@@ -15,5 +15,15 @@ So that I can provide feedback to my peer) do
       expect(page).to have_content('Great job!')
       expect(page).to have_content('Thanks for commenting')
     end
+
+    scenario 'comments on talk without comment' do
+      user = FactoryGirl.create(:user)
+      talk = FactoryGirl.create(:talk)
+      sign_in(user)
+      click_link(talk.topic)
+      fill_in 'Comment', with: ''
+      click_button 'Comment'
+      expect(page).to have_content("Comment can't be blank")
+    end
   end
 end
