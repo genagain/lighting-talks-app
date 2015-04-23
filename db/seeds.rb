7753771314fa279b97b2ae92524a99ed9ab3eb67
genagain <ohta.g@husky.neu.edu>
2015-04-23 13:40:38 -0400

    unseeding database for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'faker'
-users = []
-talks = []
+# require 'faker'
+# users = []
+# talks = []
 
-10.times do
-  users << User.create(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password(8)
-  )
-end
+# 10.times do
+  # users << User.create(
+    # email: Faker::Internet.email,
+    # password: Faker::Internet.password(8)
+  # )
+# end
 
-20.times do
-  talks << Talk.create(
-    topic: Faker::Lorem.words(2).join(' '),
-    date: Faker::Date.forward(rand(1..14)),
-    user_id: users.sample.id
-  )
-end
+# 20.times do
+  # talks << Talk.create(
+    # topic: Faker::Lorem.words(2).join(' '),
+    # date: Faker::Date.forward(rand(1..14)),
+    # user_id: users.sample.id
+  # )
+# end
 
-50.times do
-  Comment.create(
-    talk_id: talks.sample.id,
-    user_id: users.sample.id,
-    content: Faker::Lorem.sentence
-  )
+# 50.times do
+  # Comment.create(
+    # talk_id: talks.sample.id,
+    # user_id: users.sample.id,
+    # content: Faker::Lorem.sentence
+  # )
 
-end
+# end
 
 FillerWord.create(word: "[u,U]m+")
 FillerWord.create(word: "[u,U]h+")
